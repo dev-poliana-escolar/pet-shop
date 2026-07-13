d7cc14cb1ab1cf3701408169e039d3df2353d0d2
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-07-13 17:28:20 +0000

    feat: add model login e ajusta endereço

---
+++ backend/Auth/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace backend;
+
+public class LoginModel
+{
+    public string Nome { get; set; }  = string.Empty;
+    public string Senha { get; set; }  = string.Empty;
+
+}
--- backend/Models/Endereco.cs
+++ backend/Models/Endereco.cs
@@ -5,8 +5,8 @@ public class Endereco
     public int Id {get; set;}
     public int CEP {get;set;}
     public int Numero {get;set;}
-    public string Bairro  = string.Empty;
-    public string Cidade = string.Empty; 
-    public string UF = string.Empty;
-    public string Logradouro = string.Empty;
+    public string Bairro {get;set;} = string.Empty;
+    public string Cidade {get;set;} = string.Empty; 
+    public string UF {get;set;} = string.Empty;
+    public string Logradouro {get;set;} = string.Empty;
 }
