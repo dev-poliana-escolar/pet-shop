47af6f6e0e5cb13dd1a502dc5993f676c4582c42
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-07-13 17:24:21 +0000

    feat: add model de endereco #3

---
+++ backend/Models/Endereco.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models;
+
+public class Endereco
+{
+    public int Id {get; set;}
+    public int CEP {get;set;}
+    public int Numero {get;set;}
+    public string Bairro  = string.Empty;
+    public string Cidade = string.Empty; 
+    public string UF = string.Empty;
+    public string Logradouro = string.Empty;
+}
