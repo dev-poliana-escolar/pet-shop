aa8ab38261006fcf0cc39e7b0517524b7d5617ca
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-07-19 11:51:09 -0300

    refactor(model): remove classe endereço e cria classe unica Animal #12

---
+++ backend/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Models;
+
+public class Animal
+{
+    public int Id {get; set;}
+    public string Nome  {get; set;} = string.Empty;
+    public int Idade{ get; set;} 
+    public decimal Peso {get; set;} 
+    public string Especie {get;set;} = string.Empty;
+    public DateOnly DataNascimento {get;set;} 
+    public string NomeTutor {get;set;} = string.Empty;
+    public string CPF{get;set;} = string.Empty;
+    public string CEP { get; set; } = string.Empty;
+    public int Numero{get;set;} 
+    public string Bairro{get;set;} = string.Empty;
+    public string Cidade{get;set;} = string.Empty;
+    public string UF {get;set;} = string.Empty;
+    public string Logradouro{get;set;} = string.Empty;
+}
