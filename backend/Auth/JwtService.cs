940782fc65df64281f9f0e11d47399f01ccc4048
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-07-19 12:37:49 -0300

    feat(jwt):  gerar token de autorização #11

---
+++ backend/Auth/JwtService.cs
@@ -0,0 +1,46 @@
+﻿using backend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace backend.Auth;
+
+public class JwtService
+{
+    private readonly IConfiguration _configuration; // guarda uma referência às configurações do projeto
+
+    public JwtService(IConfiguration configuration) //injeção de dependência
+    {
+        _configuration = configuration; 
+    }
+
+    public string GerarToken(Usuario usuario)
+    {
+        var key = Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!);//pega a minha chave
+
+        var claims = new[]
+        {
+            // monto as "informações" que vão dentro do token
+            new Claim(JwtRegisteredClaimNames.Sub, usuario.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, usuario.Email),
+            new Claim(JwtRegisteredClaimNames.UniqueName, usuario.Nome)
+        };
+
+        //Define como o token vai ser assinado
+        var credentials = new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(
+                Convert.ToDouble(_configuration["Jwt:ExpireMinutes"])),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,39 @@
 using backend.Data;
+using backend.Auth;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<JwtService>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
 
 builder.Services.AddControllers();
 
@@ -19,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
