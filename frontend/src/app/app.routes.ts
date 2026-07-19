5cfea40debbe023400fc29994f1b36d495fd070a
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-07-19 16:46:12 -0300

    feat(angular): base de arquivos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Animais } from './animais/animais';
+//import { authGuard } from './auth/auth.guard';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: Login
+  },
+
+  {
+    path: 'register',
+    component: Register
+  },
+
+  {
+    path: 'animais',
+    component: Animais,
+  }
+
+];
